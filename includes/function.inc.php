96bf491de8b4013fdba01cd5b8a0bbfa2621bb2d
Wolf <116204495+TheWolfBro2008@users.noreply.github.com>
2023-02-08 19:51:40 +0000

    count... account sytem

--- includes/function.inc.php
+++ includes/function.inc.php
@@ -9,3 +9,44 @@
         }
         return $result;
     }
+
+    function invalidUid($username) {
+        $result;
+        if (!preg_match("/^[a-zA-Z0-9]*$/"), $username) {
+            $result = true;
+        }
+        else {
+            $result = false;
+        }
+        return $result;
+    }
+
+    function invalidEmail($email) {
+        $result;
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+            $result = true;
+        }
+        else {
+            $result = false;
+        }
+        return $result;
+    }
+
+    function pwdMatch($pwd, $pwdconfirm) {
+        $result;
+        if ($pwd !== $pwdconfirm) {
+            $result = true;
+        }
+        else {
+            $result = false;
+        }
+        return $result;
+    }
+
+    function uidExists($conn, $username, $email) {
+        $sql = "SELECT * FROM users WHERE userUid = ? OR userEmail = ?;";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare()) {
+
+        }
+    }
